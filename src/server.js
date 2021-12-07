9288b0d25e4da762f2f7283d258981d7ab166244
matheusgzamboni <matheusgzamboni@gmail.com>
2021-12-07 18:25:22 -0300

    Feito exercicio 2 - Gerando Datas

--- src/server.js
+++ src/server.js
@@ -1,32 +1,47 @@
-const express = require('express');
+const express = require("express");
 
-const app = express()
-const fileSystem = require('fs')
-const { createFolder } = require('./utils')
-app.use(express.json())
+const app = express();
+const fileSystem = require("fs");
+const { createFolder, validateMonth, datesInAMonth } = require("./utils");
+app.use(express.json());
 
-app.get('/', (req, res) => {
-   return res.status(200).json({message: 'Hello world'})
-})
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "Hello world" });
+});
 
-app.post('/',  (req, res) => {
-    const { folder, item } = req.body
-    const { job } = req.query
-    console.log(folder)
-    const existFolder = createFolder(folder)
-    delete req.body.folder
+app.post("/", (req, res) => {
+  const { folder, item } = req.body;
+  const { job } = req.query;
+  console.log(folder);
+  const existFolder = createFolder(folder);
+  delete req.body.folder;
 
-    //Este if verifica se existe o arquivo user.json
-    if (fileSystem.lstatSync('src/'+'user.json').isFile()) {
-        //Neste result, retorna os dados do JSON de dentro do user
-        const result = JSON.parse(fileSystem.readFileSync('src/'+'user.json', 'utf8'));
-        console.log(result)
-        return res.status(201).json({message: 'Caiu aqui'})
-    }
-    // //Nesta Linha de baixo, é criado o user.json e preenche o arquivo JSON de acordo com o que foi enviado no req.body
-    fileSystem.writeFileSync('src/'+'user.json', JSON.stringify(req.body));
+  //Este if verifica se existe o arquivo user.json
+  if (fileSystem.lstatSync("src/" + "user.json").isFile()) {
+    //Neste result, retorna os dados do JSON de dentro do user
+    const result = JSON.parse(
+      fileSystem.readFileSync("src/" + "user.json", "utf8")
+    );
+    console.log(result);
+    return res.status(201).json({ message: "Caiu aqui" });
+  }
+  // //Nesta Linha de baixo, é criado o user.json e preenche o arquivo JSON de acordo com o que foi enviado no req.body
+  fileSystem.writeFileSync("src/" + "user.json", JSON.stringify(req.body));
 
-    return res.status(201).json({message: 'Hello world'})
-})
+  return res.status(201).json({ message: "Hello world" });
+});
 
-app.listen(3333, () => console.log('Executando'))
+app.get("/date/:month", (req, res) => {
+  const { month } = req.params;
+  const [validMonth, monthIndex] = validateMonth(month);
+
+  if (validMonth) {
+    const dates = datesInAMonth(monthIndex);
+    return dates
+      ? res.status(200).json(dates)
+      : res.status(500).json({ message: "Ocorreu um erro durante a execução" });
+  }
+  return res.status(400).json({ message: "Mês inválido" });
+});
+
+app.listen(3333, () => console.log("Executando"));
--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,79 @@
-const fileSystem = require('fs')
+const fileSystem = require("fs");
 
-function createFolder(folderName){
-    if(!fileSystem.existsSync(folderName)){
-        fileSystem.mkdirSync(folderName)
-        return folderName + ' - criado com sucesso!'
-    }
-    return folderName + ' - não foi possível criar!'
+function createFolder(folderName) {
+  if (!fileSystem.existsSync(folderName)) {
+    fileSystem.mkdirSync(folderName);
+    return folderName + " - criado com sucesso!";
+  }
+  return folderName + " - não foi possível criar!";
+}
+
+/*
+    Função para validar se o mês obtido via parâmetro é válido.
+    O paramêtro "month" pode ser um número ou uma string que refencie o mês.
+    Para strings são aceitos o nome completo ou a abreviação. A função não é case sensitive.
+
+    A função ira retornar um array de duas posições, onde o índice 0 é um booleano indicando se o mês é válido (true) ou não (false).
+    No índice 1 é retornado um valor numérico do mês informado por parâmetro.
+
+    Ex1: validateMonth("fevereiro") => [true, 2]
+    Ex2: validateMonth("joao") => [false, 0]
+    Ex3: validateMonth(10) => [true, 10]
+*/
+function validateMonth(month) {
+  const monthsExtended = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"];
+  const monthsAbbreviation = ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"];
+  if (!isNaN(month)) {
+    return month >= 1 && month <= 12 ? [true, month] : [false, 0];
+  } else {
+    return monthsExtended.includes(month.toLowerCase())
+      ? [
+          monthsExtended.includes(month.toLowerCase()),
+          monthsExtended.indexOf(month.toLowerCase()) + 1,
+        ]
+      : [
+          monthsAbbreviation.includes(month.toLowerCase()),
+          monthsAbbreviation.indexOf(month.toLowerCase()) + 1,
+        ];
+  }
+}
+
+
+/*
+    Função que retorna um array listando todas as datas referentes ao mês pesquisado.
+    A função retorna um array ordenado de todas as datas contidas dentro de um mês.
+    O ano será sempre referente à data do sistema operacional;
+    
+    Ex: datesInAMonth(2) => [
+  "01/02/2021",
+  "02/02/2021",
+  "03/02/2021",
+  "04/02/2021",
+        .
+        .
+        .
+  "25/02/2021",
+  "26/02/2021",
+  "27/02/2021",
+  "28/02/2021"]
+*/
+function datesInAMonth(monthIndex) {
+  if(monthIndex < 1 || monthIndex > 12) {
+      return null;
+  } 
+  const year = new Date().getFullYear();
+  const date = new Date(year, monthIndex, 0);
+  const month = monthIndex < 10 ? "0" + monthIndex : "" + monthIndex;
+  let dates = [];
+  for (let i = 1; i <= date.getDate(); i++) {
+    const day = "" + i < 10 ? "0" + i : "" + i;
+    dates.push(`${day.slice(-2)}/${month.slice(-2)}/${year}`);
+  }
+  return dates;
 }
 
 module.exports = {
-    createFolder
-}
+  createFolder,
+  validateMonth,
+  datesInAMonth,
+};
